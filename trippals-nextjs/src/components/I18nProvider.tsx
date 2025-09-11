f2c9e91cbee1fc5ecf2b398ce095a5ca2f82771f
tcf01 <tsecheukfung01@gmail.com>
2025-09-11 17:19:50 +0800

    chore: get rid of the deprecated content

--- trippals-nextjs/src/components/I18nProvider.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useEffect } from 'react';
-import '@/i18n'; // Import the i18n configuration
-
-export default function I18nProvider({ children }: { children: React.ReactNode }) {
-    return <>{children}</>;
-}
